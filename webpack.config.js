8b10f867eee3ca3be6ba0adcdd56e6f74257c6ae
John Manoogian III <jm3@jm3.net>
2017-05-19 11:43:58 -0700

    handle css + less

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,8 @@ module.exports = {
   },
   module: {
     loaders: [
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' }, // use ! to chain loaders
       { test: /\.js$/,  loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
     ]
