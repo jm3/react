45b158860b16b16a6ea80634c26fe5b7419b8782
John Manoogian III <jm3@jm3.net>
2017-05-19 13:33:03 -0700

    clarify

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,8 +5,8 @@ export default class App extends React.Component {
     return (
       <div style={{textAlign: 'center'}}>
         <h1>jm3 says: Hello, React!</h1>
-        <h2>I can load CSS + LESS styles now.</h2>
-        <h2>But can I import more than one CSS file without explicitly including them all? OH HELL YES</h2>
+        <h2>webpack cfg loads CSS + LESS now.</h2>
+        <h2>Can I import many CSS files w/out explicitly js-including each?<div>OH HELL YES</div></h2>
         <h2>Behold, I shall load an external library (d3) in a chunked include.</h2>
       </div>
     );
